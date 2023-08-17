74de28177e0135db0c0233c839ad846b8b623556
Marcos José de Oliveira <marcosjose000@gmail.com>
2023-08-16 21:21:50 -0300

    aula 9.11 concluída

--- src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -19,4 +19,17 @@ public class PathExpressionTest extends EntityManagerTest {
         Assertions.assertFalse(lista.isEmpty());
 
     }
+
+    @Test
+    public void buscarPedidosComProdutoEspecifico() {
+        String jpql = "select p from Pedido p join p.itens i where i.id.produtoId = 1";
+//        String jpql = "select p from Pedido p join p.itens i where i.produto.id = 1";
+//        String jpql = "select p from Pedido p join p.itens i join i.produto pro where pro.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 2);
+
+    }
 }

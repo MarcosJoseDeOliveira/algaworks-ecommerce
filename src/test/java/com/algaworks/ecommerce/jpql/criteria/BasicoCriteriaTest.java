1ed4222e004565f471e737e897353ba6e847bc28
Marcos José de Oliveira <marcosjose000@gmail.com>
2023-09-18 18:42:38 -0300

    Aula 10.9 concluída

--- src/test/java/com/algaworks/ecommerce/jpql/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/criteria/BasicoCriteriaTest.java
@@ -20,6 +20,21 @@ import java.util.List;
 public class BasicoCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void projetaroResultadoDTO() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ProdutoDTO> criteriaQuery = criteriaBuilder.createQuery(ProdutoDTO.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(criteriaBuilder.construct(ProdutoDTO.class, root.get("id"), root.get("nome")));
+
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<ProdutoDTO> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(dto -> System.out.println("ID: " + dto.getId() + ", Nome: " + dto.getNome()));
+    }
+
+    @Test
     public void projetarOResultadoTuple() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery();
